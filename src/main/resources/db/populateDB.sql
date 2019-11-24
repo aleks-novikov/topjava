634c13a2259714eea37727845a76fac42e73a5e4
aleks-novikov <stalkerartefact@yandex.ru>
2019-11-24 17:01:21 +0300

    HW_03 added 'user_meals' table init and data populating. Implemented JdbcMealRepository

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM users;
 DELETE FROM user_roles;
+DELETE FROM user_meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,9 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO user_meals (user_id, date_time, description, calories) VALUES
+  (100000, now(), 'Завтрак', 800),
+  (100000, now(), 'Обед', 1000),
+  (100001, now(), 'Завтрак', 500),
+  (100001, now(), 'Ужин', 1200);
