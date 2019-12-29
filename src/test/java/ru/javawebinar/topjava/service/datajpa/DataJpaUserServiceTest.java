05cfea00d8d21490dd26b43e7b39958ea5c72f48
aleks-novikov <stalkerartefact@yandex.ru>
2019-12-29 23:02:19 +0300

    HW_05 added JDBC, JPA and DATA-JPA tests

---
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava.service.datajpa;
+
+import org.junit.Ignore;
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+
+//@Ignore
+//@ActiveProfiles(Profiles.DATAJPA)
+//public class DataJpaUserServiceTest extends AbstractUserServiceTest {
+//}
