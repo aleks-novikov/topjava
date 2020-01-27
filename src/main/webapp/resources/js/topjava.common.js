ab1db47c2cd5d180a964e66856f5cf70643f18fc
aleks-novikov <stalkerartefact@yandex.ru>
2020-01-28 00:01:04 +0300

    Lesson_8 added notifications

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -10,6 +10,14 @@ function makeEditable(ctx) {
             deleteRow($(this).attr("id"));
         }
     });
+
+    //обработка ошибок при выполнении ajax-запросов для всего документа
+    $(document).ajaxError(function (event, jqXHR, options, jsExc) {
+        failNoty(jqXHR);
+    });
+
+    // solve problem with cache in IE: https://stackoverflow.com/a/4303862/548473
+    $.ajaxSetup({cache: false});
 }
 
 function add() {
@@ -25,6 +33,7 @@ function deleteRow(id) {
         type: "DELETE"
     }).done(function () {
         updateTable();
+        successNoty("Deleted");
     });
 }
 
@@ -43,5 +52,34 @@ function save() {
     }).done(function () {
         $("#editRow").modal("hide");
         updateTable();
+        successNoty("Saved");
     });
+}
+
+let failedNote;
+
+function closeNoty() {
+    if (failedNote) {
+        failedNote.close();
+        failedNote = undefined;
+    }
+}
+
+function successNoty(text) {
+    closeNoty();
+    new Noty({
+        text: "<span class='fa fa-lg fa-check'></span> &nbsp;" + text,
+        type: 'success',
+        layout: "bottomRight",
+        timeout: 1000
+    }).show();
+}
+
+function failNoty(jqXHR) {
+    closeNoty();
+    failedNote = new Noty({
+        text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;Error status: " + jqXHR.status,
+        type: "error",
+        layout: "bottomRight"
+    }).show();
 }
