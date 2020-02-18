a4dd65507d749024a969c7f845c9d5403bbf98ad
aleks-novikov <stalkerartefact@yandex.ru>
2020-02-18 23:05:56 +0300

    Lesson_10 added handling 'No data found' 422 error

--- src/test/java/ru/javawebinar/topjava/web/user/AdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/AdminRestControllerTest.java
@@ -39,6 +39,13 @@ class AdminRestControllerTest extends AbstractControllerTest {
     }
 
     @Test
+    void getNotFound() throws Exception {
+        perform(doGet(1).basicAuth(ADMIN))
+                .andExpect(status().isUnprocessableEntity())
+                .andDo(print());
+    }
+
+    @Test
     void getByEmail() throws Exception {
         perform(doGet("by?email={email}", ADMIN.getEmail()).basicAuth(ADMIN))
                 .andExpect(status().isOk())
@@ -52,6 +59,13 @@ class AdminRestControllerTest extends AbstractControllerTest {
                 .andDo(print())
                 .andExpect(status().isNoContent());
         assertThrows(NotFoundException.class, () -> userService.get(USER_ID));
+    }
+
+    @Test
+    void deleteNotFound() throws Exception {
+        perform(doDelete(1).basicAuth(ADMIN))
+                .andExpect(status().isUnprocessableEntity())
+                .andDo(print());
     }
 
     @Test
